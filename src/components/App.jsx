a27f23925b2ce0f8744aa9a7330d5866e09c0185
Erlyn Ascarate <erlyndavid@outlook.es>
2022-07-18 23:26:17 -0500

    Agrega estilos al componente "Header" y quita los componentes que el archivo "App.jsx" no usará

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "../styles/global.css";
-import AddToTheList from "./AddToTheList";
 import Header from "./Header";
 
 const App = () => {
     return (
         <>
             <Header />
-            <main>
-                <AddToTheList />
-            </main>
+            <main></main>
         </>
     );
 };
