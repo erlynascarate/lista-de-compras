74abb5cf8cf5eac5ca8a4d7628844d9c452b26ca
Erlyn Ascarate <erlyndavid@outlook.es>
2022-07-20 13:05:19 -0500

    Agrega estilos al componente "ListItem"

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "../styles/ListItem.css";
 
 const ListItem = ({ id, children }) => {
     return (
-        <li>
-            <input type="checkbox" id={id} />
-            <label htmlFor={id}>{children}</label>
+        <li className="list-item">
+            <input type="checkbox" id={id} className="list-item__checkbox" />
+            <label htmlFor={id} className="list-item__name">
+                {children}
+            </label>
         </li>
     );
 };
